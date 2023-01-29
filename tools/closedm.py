a7d8257beb18bd8b088cee8e544f29da824f608d
Snee <120136010+itssnee@users.noreply.github.com>
2023-01-29 18:28:39 +0100

    Update closedm.py

--- tools/closedm.py
+++ tools/closedm.py
@@ -9,7 +9,7 @@ def dmcloser(token):
     headers = {"authorization": token, "user-agent": "Samsung Fridge/6.9"}
     close_dm_request = requests.get("https://canary.discord.com/api/v8/users/@me/channels", headers=headers).json()
     for channel in close_dm_request:
-        print(f"{Fore.RED}[ C ] ID: "+channel['id'] +)
+        print(f"{Fore.RED}[ C ] ID: "+channel['id'])
         requests.delete(
             f"https://canary.discord.com/api/v8/channels/{channel['id']}",
             headers=headers)
