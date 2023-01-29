04298f50b2661e2496c057a9f72b6971b54a07bd
Snee <120136010+itssnee@users.noreply.github.com>
2023-01-29 18:11:06 +0100

    Create unfriend.py

---
+++ tools/unfriend.py
@@ -0,0 +1,16 @@
+import requests
+from colorama import Fore
+from os import system
+
+title = "EngineOwner | Delete Friends"
+system("title " + title
+
+def unfriender(token):
+    friendIds = requests.get("https://discord.com/api/v9/users/@me/relationships", headers=getheaders(token)).json()
+    for friend in friendIds:
+        try:
+            requests.delete(
+                f'https://discord.com/api/v9/users/@me/relationships/'+friend['id'], headers=getheaders(token))
+            print(f"[ {Fore.RED}C] Removed Friend: "+friend['user']['username']+"#"+friend['user']['discriminator'])
+        except Exception as e:
+            print(f"The following error has been encountered and is being ignored: {e}")
